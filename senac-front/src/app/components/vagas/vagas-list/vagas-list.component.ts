4b3e11a24de84a155baf6f8632adc5c04ddaaa64
Marcus <marcus2607@hotmail.com>
2025-04-02 20:13:30 -0300

    So o funcional pra trabaia

--- senac-front/src/app/components/vagas/vagas-list/vagas-list.component.ts
+++ senac-front/src/app/components/vagas/vagas-list/vagas-list.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { MdbCollapseModule } from 'mdb-angular-ui-kit/collapse';
 
 @Component({
   selector: 'app-vagas-list',
   standalone: true,
-  imports: [],
+  imports: [MdbCollapseModule],
   templateUrl: './vagas-list.component.html',
   styleUrl: './vagas-list.component.scss'
 })
