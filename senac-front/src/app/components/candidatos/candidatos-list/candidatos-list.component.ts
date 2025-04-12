d59b083e0ff762de5e63e2f327af1148a49e848e
Marcelo <marcelofoztd@hotmail.com>
2025-04-11 23:07:23 -0300

    colocando buscar no candidato

--- senac-front/src/app/components/candidatos/candidatos-list/candidatos-list.component.ts
+++ senac-front/src/app/components/candidatos/candidatos-list/candidatos-list.component.ts
@@ -3,13 +3,15 @@ import { Candidato } from '../../../models/candidato';
 import { CandidatoService } from '../../../services/candidato.service';
 import Swal from 'sweetalert2';
 import { MdbModalRef, MdbModalService } from 'mdb-angular-ui-kit/modal';
+import { MdbCollapseModule } from 'mdb-angular-ui-kit/collapse';
+import { FormsModule } from '@angular/forms';
 
 
 
 @Component({
   selector: 'app-candidatos-list',
   standalone: true,
-  imports: [],
+  imports: [MdbCollapseModule,FormsModule],
   templateUrl: './candidatos-list.component.html',
   styleUrl: './candidatos-list.component.scss'
 })
@@ -17,6 +19,7 @@ export class CandidatosListComponent {
 lista: Candidato[] = [];
   pesquisa: string = "";
   CandidatoEdit!: Candidato;
+  termoBusca: string = '';
 
 
 
@@ -66,4 +69,25 @@ lista: Candidato[] = [];
 
   }
 
-}
+  buscarPorCpf() {
+    const termo = this.termoBusca?.trim();
+  
+    if (!termo) {
+      this.findAll();
+      return;
+    }
+  
+    this.candidatoService.findByCpf(termo).subscribe({
+      next: (resultado) => {
+        this.lista = Array.isArray(resultado) ? resultado : [resultado];
+      },
+      error: (error) => {
+        console.error('Erro na busca:', error);
+        this.lista = [];
+      }
+    });
+  }
+  
+  
+  }
+  
