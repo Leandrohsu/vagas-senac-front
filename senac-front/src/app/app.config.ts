5ef58c305bd8f5b8d2f645235e1a40384d931b2e
Marcus <marcus2607@hotmail.com>
2025-05-06 21:45:04 -0300

    adicionando o login da aula (decode e tals)

--- senac-front/src/app/app.config.ts
+++ senac-front/src/app/app.config.ts
@@ -3,8 +3,9 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimations } from '@angular/platform-browser/animations';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { meuhttpInterceptor } from './components/auth/http-interceptor.service';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideHttpClient(), provideRouter(routes), provideAnimations(), provideAnimations(), provideAnimations()]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideHttpClient(withInterceptors([meuhttpInterceptor])), provideRouter(routes), provideAnimations(), provideAnimations(), provideAnimations()]
 };
--- senac-front/src/app/components/layout/login/login.component.ts
+++ senac-front/src/app/components/layout/login/login.component.ts
@@ -4,12 +4,14 @@ import { MdbFormsModule } from 'mdb-angular-ui-kit/forms';
 import { Router } from '@angular/router';
 import { Login } from '../../../models/login';
 import Swal from 'sweetalert2'
+import { LoginService } from '../../auth/login.service';
+
 
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [MdbFormsModule, FormsModule],
+  imports: [MdbFormsModule, FormsModule, ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
@@ -17,16 +19,48 @@ export class LoginComponent {
   login: Login = new Login();
 
   router = inject(Router);
+  loginService = inject(LoginService);
 
-  logar(){
-    if(this.login.username == 'admin' && this.login.password == 'admin'){
-      this.router.navigate(['admin/vagas']);
-    }else{
-      Swal.fire({
-        icon: "error",
-        title: "Epa",
-        text: "C digitou o user ou senha errado animal"
-      });
-    }
+  constructor(){
+    this.loginService.removerToken();
   }
+  logar() {
+
+    this.loginService.logar(this.login).subscribe({
+      next: token => {
+
+        if (token)
+          this.loginService.addToken(token); //MUITO IMPORTANTE
+
+        this.gerarToast().fire({ icon: "success", title: "Seja bem-vindo!" });
+        this.router.navigate(['admin/dashboard']);
+
+        this.router.navigate(['/admin/carros']);
+      },
+      error: erro => {
+        Swal.fire({
+          icon: "error",
+          title: "Epa",
+          text: "C digitou o user ou senha errado animal"
+        });
+      }
+    });
+
+  }
+
+  gerarToast() {
+    return Swal.mixin({
+      toast: true,
+      position: "top-end",
+      showConfirmButton: false,
+      timer: 3000,
+      timerProgressBar: true,
+      didOpen: (toast) => {
+        toast.onmouseenter = Swal.stopTimer;
+        toast.onmouseleave = Swal.resumeTimer;
+      }
+    });
+  }
+
+
 }
